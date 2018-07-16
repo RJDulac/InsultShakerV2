a771a77d46dec747bb5b658004b51509d12f7678
Ryan Dulac <analyticrye@gmail.com>
2018-07-16 05:14:45 -0400

    put wordlists in seperate js file

--- js-src/insultShaker.js
+++ js-src/insultShaker.js
@@ -1,56 +1,5 @@
-
+//accessing dom
 const clickWord = document.getElementById("btnWord");
-
-const sillyWordList = [
-{silly: "jerk"},
-{silly: "butter"},
-{silly: "rolling"},
-{silly: "broken"},
-{silly: "booger"},
-{silly: "air head"},
-{silly: "dork"},
-{silly: "limey"},
-{silly: "klutz"},
-{silly: "nerd"},
-{silly: "dweeb"},
-{silly: "obnoxious"},
-{silly: "geezer"},
-{silly: "crusty"},
-{silly: "crazy"},
-{silly: "trashy"},
-{silly: "unkempt"},
-{silly: "beastly"},
-{silly: "sauce"},
-{silly: "busted"},
-{silly: "silly"},
-{silly: "absurd"}
-];
-
-const anotherWordList = [
-{another: "jerk"},
-{another: "butter"},
-{another: "rolling"},
-{another: "broken"},
-{another: "nugget"},
-{another: "biscuit"},
-{another: "boring"},
-{another: "cat"},
-{another: "coward"},
-{another: "trash"},
-{another: "wobbly"},
-{another: "dusty"},
-{another: "dumb"},
-{another: "fool"},
-{another: "gross"},
-{another: "stupid"}
-
-];
-
-
-
-
-
-
 
 const wordClick = document.getElementById("clickWord");
 const getRandom = document.getElementById("generatedText");
@@ -59,14 +8,12 @@ wordClick.addEventListener("click", function() {
 
 });
 
+//get random index of array
 randomWord = (mr) => (Math.floor(Math.random()*mr.length));
 
-//best way for duplicate wordlist. Puts variable output into it's own excution environment so we don't reference the same variable when invoking randomWord function
+//best way for duplicate wordlist. Puts variable output into it's own execution environment so we don't reference the same variable when invoking randomWord function
 anotherTest = () => anotherWordList[randomWord(anotherWordList)].another;
-sillyWordFunc = () =>sillyWordList[randomWord(sillyWordList)].silly;
+sillyWordFunc = () => sillyWordList[randomWord(sillyWordList)].silly;
 
  //best to use functions for using the same list twice to create unique memories
 combineWord = () => anotherTest() + " " + anotherTest() + " " + sillyWordFunc() + " " + sillyWordFunc();
-
-
-
---
+++ js-src/wordLists.js
@@ -0,0 +1,47 @@
+//store wordlists here
+
+const sillyWordList = [
+{silly: "jerk"},
+{silly: "butter"},
+{silly: "rolling"},
+{silly: "broken"},
+{silly: "booger"},
+{silly: "air head"},
+{silly: "dork"},
+{silly: "limey"},
+{silly: "klutz"},
+{silly: "nerd"},
+{silly: "dweeb"},
+{silly: "obnoxious"},
+{silly: "geezer"},
+{silly: "crusty"},
+{silly: "crazy"},
+{silly: "trashy"},
+{silly: "unkempt"},
+{silly: "beastly"},
+{silly: "sauce"},
+{silly: "busted"},
+{silly: "silly"},
+{silly: "absurd"}
+];
+
+
+const anotherWordList = [
+{another: "jerk"},
+{another: "butter"},
+{another: "rolling"},
+{another: "broken"},
+{another: "nugget"},
+{another: "biscuit"},
+{another: "boring"},
+{another: "cat"},
+{another: "coward"},
+{another: "trash"},
+{another: "wobbly"},
+{another: "dusty"},
+{another: "dumb"},
+{another: "fool"},
+{another: "gross"},
+{another: "stupid"}
+
+];
